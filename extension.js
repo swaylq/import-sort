6950f3e954658d5819568002508483dce6edc136
sway <swaylq0913@gmail.com>
2017-06-20 13:07:01 +0800

    [UPDATE] sort import basic functionality

--- extension.js
+++ extension.js
@@ -1,30 +1,79 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-var vscode = require('vscode');
+const { commands, window, Range, Position } = require('vscode');
+
+const isImportLine = (line) => {
+    const firstCharacter = getCharacter(line, 1);
+    return firstCharacter === 'import';
+};
+
+const getCharacter = (line, index) => {
+    const text = line.text;
+    const arr = text.match(/\S+/g);
+    if (!arr) return '';
+    return arr[index - 1] || '';
+};
+
+const replaceLines = (document, editorBuilder, sIndex, eIndex) => {
+    const lines = [];
+    for (let i = sIndex; i <= eIndex; i ++) {
+        lines.push(document.lineAt(i));
+    }
+    lines.sort((a, b) => {
+        if (getCharacter(a, 2) < getCharacter(b, 2)) {
+            return -1;
+        }
+        if (getCharacter(a, 2) > getCharacter(b, 2)) {
+            return 1;
+        }
+        return 0;
+    });
+    const newLines = lines.reduce((pre, line) => pre += `${line.text}\n`, '');
+    editorBuilder.replace(new Range(new Position(sIndex, 0), new Position(eIndex, document.lineAt(eIndex).text.length)), newLines.substring(0, newLines.length - 1));
+};
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-function activate(context) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
+const activate = (context) => {
     console.log('Congratulations, your extension "import-sort" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    const disposable = commands.registerCommand('extension.sort', () => {
+        const editor = window.activeTextEditor;
+        const document = editor.document;
+        const lineNumber = document.lineCount;
+        if (!editor) return;
+        editor.edit((editorBuilder) => {
+            let index = 0;
+            let currentLine;
+            let tempIndex = index;
+            // todo: annotation ignore
+            while (index < lineNumber) {
+                currentLine = document.lineAt(index);                
+                if (isImportLine(currentLine)) {
+                    tempIndex = index;
+                    while (isImportLine(currentLine) && index < lineNumber - 1) {
+                        index += 1;
+                        currentLine = document.lineAt(index);
+                    }
+                    // if it's not the end of line, index += 1
+                    if (isImportLine(currentLine)) {
+                        replaceLines(document, editorBuilder, tempIndex, index);
+                    } else {
+                        replaceLines(document, editorBuilder, tempIndex, index - 1);
+                    }
+                    index += 1;
+                } else {
+                    index += 1;
+                }
+            }
+        })
     });
-
     context.subscriptions.push(disposable);
 }
-exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {
+const deactivate = () => {
+    console.log('Extension "import-sort" is now deactivate');
 }
+
+exports.activate = activate;
 exports.deactivate = deactivate;
